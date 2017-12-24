2316da0693a71a7e0e97d729f422159aabdbf740
Tomasz Kula <tomaszkula07@gmail.com>
2017-12-24 16:45:09 +0100

    Add indexDB abstract class

---
+++ src/app/classes/indexdb.ts
@@ -0,0 +1,86 @@
+import idb, { DB, UpgradeDB } from 'idb';
+
+export abstract class IndexDB<T> {
+  private db: Promise<DB>;
+
+  abstract readonly dbName: string;
+  abstract readonly storeName: string;
+  abstract readonly upgradeCallback: (db: UpgradeDB) => void;
+
+  get dbPromise(): Promise<DB> {
+    if (!this.db) {
+      this.db = idb.open(this.dbName, 1, this.upgradeCallback.bind(this));
+    }
+    return this.db;
+  }
+
+  constructor() {}
+
+  getAll(): Promise<T[]> {
+    return this.dbPromise.then(db => {
+      return db
+        .transaction(this.storeName)
+        .objectStore(this.storeName)
+        .getAll();
+    });
+  }
+
+  get(key: keyof T): Promise<T> {
+    return this.dbPromise.then(db => {
+      return db
+        .transaction(this.storeName)
+        .objectStore(this.storeName)
+        .get(key);
+    });
+  }
+
+  add(val: Partial<T>, key?: keyof T): Promise<void> {
+    return this.dbPromise.then(db => {
+      const tx = db.transaction(this.storeName, 'readwrite');
+      tx.objectStore(this.storeName).add(val, key);
+      return tx.complete;
+    });
+  }
+
+  set(key: keyof T, val: Partial<T>) {
+    return this.dbPromise.then(db => {
+      const tx = db.transaction(this.storeName, 'readwrite');
+      tx.objectStore(this.storeName).put(val, key);
+      return tx.complete;
+    });
+  }
+
+  delete(key: keyof T) {
+    return this.dbPromise.then(db => {
+      const tx = db.transaction(this.storeName, 'readwrite');
+      tx.objectStore(this.storeName).delete(key);
+      return tx.complete;
+    });
+  }
+
+  clear() {
+    return this.dbPromise.then(db => {
+      const tx = db.transaction(this.storeName, 'readwrite');
+      tx.objectStore(this.storeName).clear();
+      return tx.complete;
+    });
+  }
+
+  keys() {
+    return this.dbPromise.then(db => {
+      const tx = db.transaction(this.storeName);
+      const keys = [];
+      const store = tx.objectStore(this.storeName);
+
+      (store.iterateKeyCursor || store.iterateCursor).call(store, cursor => {
+        if (!cursor) {
+          return;
+        }
+        keys.push(cursor.key);
+        cursor.continue();
+      });
+
+      return tx.complete.then(() => keys);
+    });
+  }
+}
